b798df48294fb2ecef6eb17dad2845e8f4d00301
Dér Zsombor Zsolt <zsomborder@gmail.com>
2025-02-23 17:43:14 +0100

    Add project files.

---
+++ GameOfLifeTDD/GameOfLife/Game.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeTDD.GameOfLife
+{
+    public class Game
+    {
+        public const int DEFAULT_WIDTH = 16;
+        public const int DEFAULT_HEIGHT = 16;
+        private bool[,] matrix;
+
+        public int Height => matrix.GetLength(0);
+        public int Width => matrix.GetLength(1);
+        public bool GetCell(int row, int col) => matrix[row, col];
+
+        public Game(int height = DEFAULT_HEIGHT, int width = DEFAULT_WIDTH)
+        {
+            if (height <= 0)
+            {
+                throw new ArgumentException("Can't have non positive height", nameof(height));
+            }
+            if (width <= 0)
+            {
+                throw new ArgumentException("Can't have non positive width", nameof(width));
+            }
+            this.matrix = new bool[height, width];
+        }
+
+        public void SetCells(List<(int, int)> cells)
+        {
+            foreach (var cell in cells)
+            {
+                if (cell.Item1 < 0 || cell.Item1 >= Height || cell.Item2 < 0 || cell.Item2 >= Width)
+                {
+                    throw new ArgumentException("Cell was out of bounds", nameof(cells));
+                }
+
+                this.matrix[cell.Item1, cell.Item2] = true;
+            }
+        }
+
+
+        /// <summary>
+        /// Run the simulation
+        /// </summary>
+        /// <param name="generationCount">If negative number was given run infinitely</param>
+        /// <param name="animate">Should it wait between generations</param>
+        /// <returns></returns>
+        public async Task Run(int generationCount, bool animate = false)
+        {
+
+            while (generationCount != 0)
+            {
+                if (generationCount > 0)
+                {
+                    generationCount--;
+                }
+
+
+
+                if (animate)
+                {
+                    await Task.Delay(100);
+                }
+            }
+        }
+
+        private void Tick()
+        {
+            bool[,] newBoard = new bool[Height, Width];
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    int neighbourCount = NeighbourCount(i, j);
+                    if (matrix[i, j])
+                    {
+                        newBoard[i,j]= neighbourCount ==2 || neighbourCount ==3;
+                    }
+                    else
+                    {
+                        newBoard[i, j] = neighbourCount == 3;
+                    }
+                }
+            }
+
+            this.matrix = newBoard;
+        }
+
+        private int NeighbourCount(int row, int col)
+        {
+            int counter = 0;
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                if (i >= Height || i < 0)
+                {
+                    continue;
+                }
+                for (int j = col - 1; j <= col + 1; j++)
+                {
+                    if (j >= Width || j < 0 || (i == row && j == col))
+                    {
+                        continue;
+                    }
+                    if (matrix[i, j])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        public void ClearBoard()
+        {
+            this.matrix = new bool[Height,Width];
+        }
+
+        public void ImportRLEFile(string filePath)
+        {
+            string fileContent= File.ReadAllText(filePath, Encoding.UTF8);
+
+
+
+
+        }
+    }
+}
---
+++ Tests/GameBaseFunctionalityTests.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using GameOfLifeTDD.GameOfLife;
+
+namespace Tests
+{
+    public class GameBaseFunctionalityTests
+    {
+
+        [Theory]
+        [InlineData(10, 10)]
+        [InlineData(10, 5)]
+        [InlineData(5, 10)]
+        public void CreateTest(int height, int width)
+        {
+            Game game = new Game(height, width);
+            Assert.Equal(height, game.Height);
+            Assert.Equal(width, game.Width);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Assert.False(game.GetCell(i, j));
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-4)]
+        public void InvalidHeightTest(int height)
+        {
+            Assert.Throws<ArgumentException>(() => new Game(height));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-4)]
+        public void InvalidWidthTest(int width)
+        {
+            Assert.Throws<ArgumentException>(() => new Game(width: width));
+        }
+
+        [Fact]
+        public void DefaultGameSize()
+        {
+            Game game = new Game();
+            Assert.Equal(Game.DEFAULT_HEIGHT, game.Height);
+            Assert.Equal(Game.DEFAULT_WIDTH, game.Width);
+        }
+
+        [Fact]
+        public void SetCellsTest1()
+        {
+            Game game = new Game();
+            List<(int, int)> cells = new List<(int, int)>() { (5, 5), (1, 4), (1, 6), (1, 7), (1, 8), (1, 9), (1, 10) };
+            game.SetCells(cells);
+
+            foreach (var cell in cells)
+            {
+                Assert.True(game.GetCell((int)cell.Item1, (int)cell.Item2));
+            }
+        }
+
+        [Fact]
+        public void SetCellsTestOutOfRangeTest1()
+        {
+            Game game = new Game();
+            List<(int, int)> cells = new List<(int, int)>() { (5, 5), (1, 4), (1, 6), (1, 7), (1, 8), (1, 9), (1, 16) };
+            Assert.Throws<ArgumentException>(() => game.SetCells(cells));
+        }
+
+        [Fact]
+        public void SetCellsTestOutOfRangeTest2()
+        {
+            Game game = new Game();
+            List<(int, int)> cells = new List<(int, int)>() { (5, 5), (1, 4), (1, 6), (1, 7), (1, 8), (1, 9), (16, 1) };
+            Assert.Throws<ArgumentException>(() => game.SetCells(cells));
+        }
+
+        [Fact]
+        public void GetNeighbourCountTest1()
+        {
+            Game game = new Game();
+
+            List<(int, int)> cells = new List<(int, int)>() { (3, 3), (2, 3), (4, 3), (3, 4), (3, 2) };
+            game.SetCells(cells);
+
+            MethodInfo methodInfo = typeof(Game).GetMethod("NeighbourCount", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { 3, 3 };
+            int? count = (int?)methodInfo?.Invoke(game, parameters);
+            Assert.Equal(4, count);
+        }
+
+
+        [Fact]
+        public void GetNeighbourCountTest2()
+        {
+            Game game = new Game();
+
+            List<(int, int)> cells = new List<(int, int)>() { (3, 0), (3, 1), (2, 0), (4, 0), (4, 2) };
+            game.SetCells(cells);
+
+            MethodInfo methodInfo = typeof(Game).GetMethod("NeighbourCount", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { 3, 0 };
+            int? count = (int?)methodInfo?.Invoke(game, parameters);
+            Assert.Equal(3, count);
+        }
+
+        [Fact]
+        public void GetNeighbourCountTest3()
+        {
+            Game game = new Game();
+
+            List<(int, int)> cells = new List<(int, int)>() { (3, 0), (3, 1), (2, 0), (4, 0), (4, 2) };
+            game.SetCells(cells);
+
+            MethodInfo methodInfo = typeof(Game).GetMethod("NeighbourCount", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { -1, -1 };
+            int? count = (int?)methodInfo?.Invoke(game, parameters);
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void GameTickTest1()
+        {
+            Game game = new Game();
+            List<(int, int)> cells = new List<(int, int)>() { (3, 3), (3, 2), (3, 4) };
+            game.SetCells(cells);
+
+            MethodInfo methodInfo = typeof(Game).GetMethod("Tick", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            methodInfo?.Invoke(game, parameters);
+
+            Assert.True(game.GetCell(3, 3));
+            Assert.True(game.GetCell(4, 3));
+            Assert.True(game.GetCell(2, 3));
+            methodInfo?.Invoke(game, parameters);
+            Assert.True(game.GetCell(3, 3));
+            Assert.True(game.GetCell(3, 4));
+            Assert.True(game.GetCell(3, 2));
+        }
+
+        [Fact]
+        public void GameTickTest2()
+        {
+            Game game = new Game();
+            List<(int, int)> cells = new List<(int, int)>() { (3, 3) };
+            game.SetCells(cells);
+
+            MethodInfo methodInfo = typeof(Game).GetMethod("Tick", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            methodInfo?.Invoke(game, parameters);
+
+            Assert.False(game.GetCell(3, 3));
+            methodInfo?.Invoke(game, parameters);
+            Assert.False(game.GetCell(3, 3));
+        }
+
+        [Fact]
+        public async Task GameRunTest1()
+        {
+            Game game = new Game();
+            List<(int, int)> cells = new List<(int, int)>() { (3, 3), (2, 2) };
+            game.SetCells(cells);
+
+            await game.Run(10);
+            Assert.True(game.GetCell(2, 2));
+            Assert.True(game.GetCell(3, 3));
+            Assert.False(game.GetCell(2, 3));
+            Assert.False(game.GetCell(3, 2));
+
+
+        }
+
+    }
+}
